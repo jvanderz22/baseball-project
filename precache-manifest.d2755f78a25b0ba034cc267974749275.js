70600dac26259cba2a16d2269e9fb51e1f48962c
Jordan VanderZwaag <vanderzwaagjordan@gmail.com>
2019-06-21 08:25:10 -0500

    Updates

---
+++ precache-manifest.d2755f78a25b0ba034cc267974749275.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bddf2b36cf89f85503010a7b236b390d",
+    "url": "/baseball-project/index.html"
+  },
+  {
+    "revision": "8d633d868a7df0c92b87",
+    "url": "/baseball-project/static/css/main.b0ed1945.chunk.css"
+  },
+  {
+    "revision": "4742b19dd0317407066d",
+    "url": "/baseball-project/static/js/2.e3123218.chunk.js"
+  },
+  {
+    "revision": "8d633d868a7df0c92b87",
+    "url": "/baseball-project/static/js/main.f0b86a27.chunk.js"
+  },
+  {
+    "revision": "e7a61313be3753a955c1",
+    "url": "/baseball-project/static/js/runtime~main.4a24f26b.js"
+  }
+]);
