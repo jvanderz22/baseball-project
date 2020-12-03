5ca9baa94009c3646b707e4d9606d73f316688dc
Jordan VanderZwaag <vanderzwaagjordan@gmail.com>
2020-12-03 10:43:17 -0600

    Editted release

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.scss'
 import App from './app'
 import * as serviceWorker from './serviceWorker'
 
+console.log('here!!!')
 ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
